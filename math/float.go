28548a1b41e916b6ade779256d21f30b775cb866
PureDu <dxbinlive@gmail.com>
2018-02-04 16:06:37 +0800

    math: IsFloat32Equal IsFloat64Equal

--- math/float.go
+++ math/float.go
@@ -14,3 +14,11 @@ func IsZeroFloat32(value float32) bool {
 func IsZeroFloat64(value float64) bool {
 	return math.Abs(value) < FLOAT64EPSILON
 }
+
+func IsFloat64Equal(l, r float64) bool {
+	return math.Abs(l-r) < FLOAT64EPSILON
+}
+
+func IsFloat32Equal(l, r float32) bool {
+	return IsFloat64Equal(float64(l), float64(r))
+}
