bdda117ae75745259aab2a9dee7a51a627248aff
PureDu <dxbinlive@gmail.com>
2018-02-04 17:21:19 +0800

    GUID method

--- data/GUID.go
+++ data/GUID.go
@@ -1,6 +1,35 @@
 package data
 
+import "fmt"
+
 type GUID struct {
-	High uint64
-	LOW  uint64
+	high uint64
+	low  uint64
+}
+
+func NewGUID(high, low uint64) GUID {
+	return GUID{
+		high: high,
+		low:  low,
+	}
+}
+
+func NewNULLGUID() GUID {
+	return NewGUID(0, 0)
+}
+
+func (g *GUID) GetHigh() uint64 {
+	return g.high
+}
+
+func (g *GUID) GetLow() uint64 {
+	return g.low
+}
+
+func (g *GUID) IsNULL() bool {
+	return (g.high == 0) && (g.low == 0)
+}
+
+func (g *GUID) String() string {
+	return fmt.Sprintf("%d-%d", g.high, g.low)
 }
